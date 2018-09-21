edc8acd844578934739c10042fc1687ba6180d1c
Tim Simmons <tim.r.simmons@gmail.com>
2018-09-21 09:35:13 -0500

    it works!
    I needed to understand how `?` worked a bit more and that
    the failure::Error type worked with `?` but I'd still need to
    return an `Ok(string)`. I get it now!
    
    now with an invalid bit of json it does this the program prints:
    ```
    error: key must be a string,
    ```
    
    yay

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
 
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -13,6 +14,7 @@ use failure::Error;
 struct Point {
     x: i32,
     y: i32,
+    m: HashMap<(i32, i32), bool>,
 }
 
 fn main() {
@@ -23,10 +25,13 @@ fn main() {
         Err(e) => panic!(e),
     }
 
-    let point = Point { x: 1, y: 2 };
+    let mut m: HashMap<(i32, i32), bool> = HashMap::new();
+
+    m.insert((1, 1), true);
+    let point = Point { x: 1, y: 2, m: m };
     match point_to_json(point) {
         Ok(s) => println!("{}", s),
-        Err(e) => panic!(e),
+        Err(e) => println!("error: {}, {}", e.as_fail(), e.backtrace()),
     }
 }
 
@@ -37,5 +42,6 @@ fn open(path: &str) -> Result<String, Error> {
 }
 
 fn point_to_json(point: Point) -> Result<String, Error> {
-    serde_json::to_string(&point)?
+    let s = serde_json::to_string(&point)?;
+    Ok(s)
 }
