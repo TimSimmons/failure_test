057dbaa16bfcabf7544bb8c8b5d3d1aa73de1315
Tim Simmons <tim.r.simmons@gmail.com>
2018-09-20 22:22:39 -0500

    but this doesn't work
    and I don't know why :\ this is weird:
    ```
      --> src/main.rs:40:5
       |
    40 |     serde_json::to_string(&point)?
       |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
       |     |
       |     expected enum `std::result::Result`, found struct `std::string::String`
       |     match arm with an incompatible type
       |
       = note: expected type `std::result::Result<std::string::String, failure::Error>`
                  found type `std::string::String`
    ```
    
    because it definitely returns a Result<String, serde_json::Error>
    if you change it so that you assign to a variable you get this:
    
    ```
      --> src/main.rs:41:5
       |
    39 | fn point_to_json(point: Point) -> Result<String, Error> {
       |                                   --------------------- expected `std::result::Result<std::string::String, failure::Error>` because of return type
    40 |     let s = serde_json::to_string(&point);
    41 |     s
       |     ^ expected struct `failure::Error`, found struct `serde_json::Error`
       |
       = note: expected type `std::result::Result<_, failure::Error>`
                  found type `std::result::Result<_, serde_json::Error>`
    ```

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
 extern crate failure;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
 
 use std::fs::File;
 use std::io::prelude::*;
 
 use failure::Error;
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
 fn main() {
     println!("Hello, world!");
     let path = "example.txt";
     match open(path) {
+        Ok(s) => println!("{}", s),
+        Err(e) => panic!(e),
+    }
+
+    let point = Point { x: 1, y: 2 };
+    match point_to_json(point) {
         Ok(s) => println!("{}", s),
         Err(e) => panic!(e),
     }
@@ -18,4 +34,8 @@ fn open(path: &str) -> Result<String, Error> {
     let mut string = String::new();
     File::open(path)?.read_to_string(&mut string)?;
     Ok(string)
+}
+
+fn point_to_json(point: Point) -> Result<String, Error> {
+    serde_json::to_string(&point)?
 }
